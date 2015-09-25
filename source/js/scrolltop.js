dc707115657d1c22dfcbc3ec95105994045175c7
ForestCho <caosl158@gmail.com>
2015-09-25 23:05:16 +0800

    增加图片查看，nprogress,删除fancybox

--- source/js/scrolltop.js
+++ source/js/scrolltop.js
@@ -1,6 +1,5 @@
-// JavaScript Document
-function goTopEx(){
-        var obj=document.getElementById("goTopBtn");
+$(document).ready(function(){
+     var gotopobj=document.getElementById("goTopBtn");
         function getScrollTop(){
                 return document.documentElement.scrollTop|document.body.scrollTop;
             }
@@ -8,36 +7,33 @@ function goTopEx(){
                 document.documentElement.scrollTop=value;
                 document.body.scrollTop=value;
             }    
-        window.onscroll=function(){getScrollTop()>60?obj.style.display="":obj.style.display="none";}
-        obj.onclick=function(){
+        window.onscroll=function(){getScrollTop()>60?gotopobj.style.display="":gotopobj.style.display="none";}
+        gotopobj.onclick=function(){
             var goTop=setInterval(scrollMove,10);
             function scrollMove(){
                     setScrollTop(getScrollTop()/1.1);
                     if(getScrollTop()<1)clearInterval(goTop);
                 }
         }
-    }
 
-// JavaScript Document
 
-function displaySide(){
         var obj = $('#displayside');
-		var imgobj = $('#imgaside');
-		var sideobj = $('#asidepart');
-		var mainobj = $('#main'); 
-        obj.click(function(){	
+        var imgobj = $('#imgaside');
+        var sideobj = $('#asidepart');
+        var mainobj = $('#main'); 
+        obj.click(function(){   
                if(sideobj.length > 0){
-		    if(sideobj.css("display") != 'none'){
-				imgobj.attr("title","显示侧边栏");    
-				imgobj.attr("src","/img/side.png");   
-				sideobj.css("display","none"); 
-				mainobj.addClass("nodisp").css("width","90%").css("float","none").css("margin-left","5%").css("margin-top","1em");  
-			}else{
-				imgobj.attr("title","隐藏侧边栏");    	
-				imgobj.attr("src","/img/noside.png");    			
-				sideobj.css("display","");  
-				mainobj.addClass("disp").css("width","75%").css("float","left").css("margin","1em 0 0");  
-			}
+            if(sideobj.css("display") != 'none'){
+                imgobj.attr("title","显示侧边栏");    
+                imgobj.attr("src","/img/side.png");   
+                sideobj.css("display","none"); 
+                mainobj.addClass("nodisp").css("width","90%").css("float","none").css("margin-left","5%").css("margin-top","1em");  
+            }else{
+                imgobj.attr("title","隐藏侧边栏");       
+                imgobj.attr("src","/img/noside.png");               
+                sideobj.css("display","");  
+                mainobj.addClass("disp").css("width","72%").css("float","left").css("margin","1em 0 0");  
+            }
                 }
-		})
-    }
+        })
+}); 
